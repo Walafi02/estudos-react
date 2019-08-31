d3777e2b21271987177efb3fe93cf485ebdb1462
Walafi02 <walafif@yahoo.com>
2019-08-30 22:37:02 -0300

    Implementando o frontend

--- todo-app/frontend/src/todo/todo.js
+++ todo-app/frontend/src/todo/todo.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import PegeHeader from '../template/pageHeader'
+import TodoForm from './todoForm'
+import TodoList from './todoList'
 
 export default class Todo extends Component{
 
@@ -7,6 +9,8 @@ export default class Todo extends Component{
         return (
             <div>
                 <PegeHeader name="Tarefas" small="Cadastro" />
+                <TodoForm />
+                <TodoList />
             </div>
         )
     }
