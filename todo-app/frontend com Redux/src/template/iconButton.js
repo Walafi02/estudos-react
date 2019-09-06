35fc8e7f55d56180cc6905b46f9a1943992fc35a
Walafi02 <walafif@yahoo.com>
2019-09-05 21:41:06 -0300

    Iniciando o projeto do Frontend com Redux

---
+++ todo-app/frontend com Redux/src/template/iconButton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import If from './if'
+
+export default props => (
+    <If test={props.hide}>
+        <button className={'btn btn-' +props.style} onClick={props.click}>
+            <i className={'fa fa-' + props.icon} onClick={props.onClick}></i>
+        </button>
+    </If>
+
+)
