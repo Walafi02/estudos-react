c625709d4a1768a729c5310aba9f6652e20d6488
Walafi02 <walafif@yahoo.com>
2019-09-08 18:40:08 -0300

    Iniciando o projeto my-money-app

---
+++ my-money-app/backend/src/api/billingCycle/billingCycle.js
@@ -0,0 +1,56 @@
+const restFull = require('node-restful')
+const mongoose = restFull.mongoose
+
+const creditSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    value: {
+        type: Number,
+        min: 0,
+        required: true
+    }
+})
+
+const debtSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    value: {
+        type: Number,
+        min: 0,
+        required: true
+    }
+    ,
+    status: {
+        type: String,
+        required: true,
+        uppercase: true,
+        enum: ['PAGO', 'PEDENTE', 'AGENDADO']
+    }
+})
+
+const billingCycleScama = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    month: {
+        type: Number,
+        min: 1,
+        max: 12,
+        required: true
+    },
+    year: {
+        type: Number,
+        min: 1970,
+        max: 2100,
+        required: true
+    },
+    credits: [creditSchema],
+    debts: [debtSchema]
+})
+
+module.exports = restFull.model('BillingCycle', billingCycleScama)
