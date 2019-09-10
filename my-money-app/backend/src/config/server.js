f71850400b13231a6260386670512f65a9a7a92e
Walafi02 <walafif@yahoo.com>
2019-09-09 22:58:40 -0300

    Finalizando o backend

--- my-money-app/backend/src/config/server.js
+++ my-money-app/backend/src/config/server.js
@@ -1,11 +1,15 @@
 const bodyParser = require('body-parser')
 const express = require('express')
+const cors = require('./cors')
+const queryParser = require('express-query-int')
 
 const server = express()
 const port = 3003
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(cors)
+server.use(queryParser())
 
 server.listen(port, function(){
     console.log(`Servidor rodando na porta ${port}`)
